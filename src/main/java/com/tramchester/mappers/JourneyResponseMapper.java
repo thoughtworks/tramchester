9c82cdb92f81e1a2b3c43a6f5e3873e6d0c5d37c
Nima <nima@nimamontazeri.com>
2015-02-24 14:15:05 +0000

    Adds stations to journey representation

--- src/main/java/com/tramchester/mappers/JourneyResponseMapper.java
+++ src/main/java/com/tramchester/mappers/JourneyResponseMapper.java
@@ -1,12 +1,22 @@
 package com.tramchester.mappers;
 
 import com.tramchester.domain.Journey;
+import com.tramchester.domain.Station;
+import com.tramchester.domain.TransportData;
 import com.tramchester.representations.JourneyPlanRepresentation;
 
 import java.util.List;
 
 public class JourneyResponseMapper {
+
+    private TransportData transportData;
+
+    public JourneyResponseMapper(TransportData transportData) {
+        this.transportData = transportData;
+    }
+
     public JourneyPlanRepresentation map(List<Journey> journeys) {
+        List<Station> stations = 
         return new JourneyPlanRepresentation(journeys, null);
     }
 }
