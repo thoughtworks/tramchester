b104b4813debd7150ea0f9326b12c8af0ba62328
Ian Cartwright <icartwright@thoughtworks.com>
2015-06-23 23:40:44 +0100

    Fix failing tests due to setup data and test data files

--- src/test/unit/com/tramchester/dataimport/parsers/FeedInfoDataParserTest.java
+++ src/test/unit/com/tramchester/dataimport/parsers/FeedInfoDataParserTest.java
@@ -7,13 +7,17 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FeedInfoDataParserTest {
-    String feedInfo = "feed_valid_from,feed_valid_to,feed_version";
+    String feedInfo = "feed_publisher_name,feed_publisher_url,feed_timezone,feed_lang,feed_valid_from,feed_valid_to,feed_version";
 
     @Test
     public void shouldParserFeedInfo() {
         FeedInfoDataParser feedInfoDataParser = new FeedInfoDataParser();
 
         FeedInfo info = feedInfoDataParser.parseEntry(this.feedInfo.split(","));
+        assertThat(info.getPublisherName()).isEqualTo("feed_publisher_name");
+        assertThat(info.getPublisherUrl()).isEqualTo("feed_publisher_url");
+        assertThat(info.getTimezone()).isEqualTo("feed_timezone");
+        assertThat(info.getLang()).isEqualTo("feed_lang");
         assertThat(info.getVersion()).isEqualTo("feed_version");
         assertThat(info.validFrom()).isEqualTo("feed_valid_from");
         assertThat(info.validUntil()).isEqualTo("feed_valid_to");
