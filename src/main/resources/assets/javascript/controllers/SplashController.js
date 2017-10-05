1dfb3b63269201e876cb35bb6e5c40fef1ce0f1a
Ian Cartwright <cartwright.ian@gmail.com>
2017-10-05 16:27:56 +0100

    Give visited cookie same expiry as for recent stops

--- src/main/resources/assets/javascript/controllers/SplashController.js
+++ src/main/resources/assets/javascript/controllers/SplashController.js
@@ -7,6 +7,8 @@ techLabApp.controller('SplashController',
         };
         $scope.setCookie = function () {
             var cookie = { 'visited' : true };
-            $cookies.putObject('tramchesterVisited',cookie);
+            var expiry = moment().add(100, 'days').toDate();
+
+            $cookies.putObject('tramchesterVisited',cookie, {'expires': expiry});
         }
     });
