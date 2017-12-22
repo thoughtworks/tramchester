fe67a1e73b0363af9aadf789b8899d5b1ad7f942
Ian Cartwright <cartwright.ian@gmail.com>
2017-12-22 13:51:20 +0000

    Use headless chrome for tests, except for geolocation ones where headless doesn't seem to support access to location

--- src/test/com/tramchester/acceptance/infra/ProvidesChromeDriver.java
+++ src/test/com/tramchester/acceptance/infra/ProvidesChromeDriver.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 
 public class ProvidesChromeDriver extends ProvidesDesktopDriver {
     private final DesiredCapabilities capabilities;
+    private final ChromeOptions chromeOptions;
     private LatLong location;
 
     public ProvidesChromeDriver(boolean enableGeo) {
@@ -18,11 +19,26 @@ public class ProvidesChromeDriver extends ProvidesDesktopDriver {
         if (chromedriverPath!=null) {
             System.setProperty("webdriver.chrome.driver",chromedriverPath);
         }
+
         capabilities = createCapabilities(enableGeo);
+
+        chromeOptions = new ChromeOptions();
+        if (!enableGeo) {
+            // geolocation in headless doesn't seem to work
+            chromeOptions.addArguments("--headless");
+        }
+
+        if (!enableGeo) {
+            chromeOptions.addArguments("--disable-geolocation");
+        } else {
+            chromeOptions.addArguments("--enable-geolocation");
+        }
     }
 
     @Override
     public void init() {
+
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         ChromeDriver chromeDriver = new ChromeDriver(capabilities);
         driver = chromeDriver;
         driver.manage().deleteAllCookies();
