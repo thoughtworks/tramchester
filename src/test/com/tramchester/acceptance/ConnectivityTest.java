f57d913adbda95b7e9051cad6d51257578ea8167
Ian Cartwright <cartwright.ian@gmail.com>
2018-03-27 16:21:41 +0100

    Add test that checks for server connectivity on port 80 and 443, in part to try and diagnose continuing test failures on travis ci

---
+++ src/test/com/tramchester/acceptance/ConnectivityTest.java
@@ -0,0 +1,44 @@
+package com.tramchester.acceptance;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URI;
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
+
+public class ConnectivityTest {
+
+    @Test
+    public void checkCanReachServer() {
+        Optional<String> maybeUrl = Optional.ofNullable(System.getenv("SERVER_URL"));
+
+        if (!maybeUrl.isPresent()) {
+            return;
+        }
+
+        URI uri = URI.create(maybeUrl.get());
+        String host = uri.getHost();
+
+        boolean https = isReachable(host, 443);
+        assertTrue(https);
+
+        boolean http = isReachable(host, 80);
+        assertTrue(http);
+
+    }
+
+    private boolean isReachable(String host, int port) {
+        boolean opened = false;
+        try {
+            Socket socket = new Socket(host, port);
+            opened = true;
+            socket.close();
+        } catch (IOException e) {
+            // can't open socket
+        }
+        return opened;
+    }
+}
