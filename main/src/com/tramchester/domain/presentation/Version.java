3e03c6060d5fb62103d9700e5e9fbcb72dfb0015
Ian Cartwright <cartwright.ian@gmail.com>
2019-07-16 12:13:21 +0100

    Fix test that failed due to mismatch on getter and fieldname breaking JSON de-serialisation

--- main/src/com/tramchester/domain/presentation/Version.java
+++ main/src/com/tramchester/domain/presentation/Version.java
@@ -3,18 +3,18 @@ package com.tramchester.domain.presentation;
 public class Version {
     public static String MajorVersion = "2";
 
-    private String versionNumber;
+    private String buildNumber;
 
     public Version() {
         // deserialization
     }
 
-    public Version(String versionNumber) {
-        this.versionNumber = versionNumber;
+    public Version(String buildNumber) {
+        this.buildNumber = buildNumber;
     }
 
     // from json
     public String getBuildNumber() {
-        return versionNumber;
+        return buildNumber;
     }
 }
