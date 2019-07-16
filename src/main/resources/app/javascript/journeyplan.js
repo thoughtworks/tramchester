ee4cb7f0071265d971f0f21fbd008ab0aea4ca11
Ian Cartwright <cartwright.ian@gmail.com>
2019-07-16 16:44:39 +0100

    Very simple input and query working with new front-end code

--- src/main/resources/app/javascript/journeyplan.js
+++ src/main/resources/app/javascript/journeyplan.js
@@ -3,14 +3,38 @@ new Vue({
         el: '#journeyplan',
         data () {
             return {
-                message: 'Hello journeyplan!',
-                items: null
+                stops: null,
+                startStop: null,
+                endStop: null,
+                time: null,
+                date: null
             }
         },
+        methods: {
+            plan(){
+                axios.get('http://localhost:8080/api/journey', {
+                    params: {
+                        start: this.startStop, end: this.endStop, departureTime: this.time, departureDate: this.date}
+                }).then(function (response) {
+                    // handle success
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+                
+            }
+        }
+        ,
         mounted () {
             axios
                 .get('http://localhost:8080/api/stations')
                 .then(response => (
-                    this.items = response.data.stations))
+                    this.stops = response.data.stations))
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
         }
     })
