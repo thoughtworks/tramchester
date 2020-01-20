ad451a1c7b0cbe52ed678759163ee9f318080f3c
Ian Cartwright <cartwright.ian@gmail.com>
2020-01-20 15:24:01 +0000

    Refactoring to clean up code

--- src/main/resources/app/javascript/map.js
+++ src/main/resources/app/javascript/map.js
@@ -1,5 +1,4 @@
 
-
 const axios = require('axios');
 var _ = require('lodash');
 var Vue = require('vue');
@@ -14,6 +13,7 @@ import './../css/tramchester.css'
 
 var width = 800;
 var height = 800;
+var margin = 24;
 
 function initD3(app) {
     app.svg = d3.select("#map").append("svg")
@@ -25,49 +25,67 @@ const mapApp = new Vue({
     el: '#tramMap',
     data() {
         return {
-            positions: null,
+            positionsList: null,
+            uniqueStations: [],
             networkError: false,
             projection: null,
             path: null,
-            svg: null
+            svg: null,
+            lonOffset: 0,
+            latOffset: 0,
+            scaleLon: 0,
+            scaleLat: 0
         }
     },
     methods: {
-        draw() {
+        filterPositions() {
+            var ids = [];
+            mapApp.positionsList.forEach(item => {
+                if (ids.indexOf(item.first.id)<0) {
+                    ids.push(item.first.id);
+                    mapApp.uniqueStations.push(item.first);
+                }
+                if (ids.indexOf(item.second.id)<0) {
+                    ids.push(item.second.id);
+                    mapApp.uniqueStations.push(item.second);
+                }
+            });
             let minLat = 1000;
             let maxLat = -1000;
             let minLon = 1000;
             let maxLon = -1000;
-            mapApp.positions.positionsList.forEach(position => {
-                var lat = position.first.latLong.lat;
+            mapApp.uniqueStations.forEach(position => {
+                var lat = position.latLong.lat;
                 if (lat<minLat) {
                     minLat = lat;
                 } else if (lat>maxLat) {
                     maxLat = lat;
                 }
-                var lon = position.first.latLong.lon;
+                var lon = position.latLong.lon;
                 if (lon<minLon) {
                     minLon = lon;
                 } else if (lon>maxLon) {
                     maxLon = lon;
                 }
             });
-            let lonOffset = 0-minLon;
-            let latOffset = 0-minLat;
-            let scaleLon = width / (maxLon-minLon);
-            let scaleLat = height / (maxLat-minLat);
+            mapApp.lonOffset = 0-minLon;
+            mapApp.latOffset = 0-minLat;
+            mapApp.scaleLon = (width-margin) / (maxLon-minLon);
+            mapApp.scaleLat = (height-margin) / (maxLat-minLat);
+        },
+        draw() {
             mapApp.svg.selectAll("circle")
-                .data(mapApp.positions.positionsList)
+                .data(mapApp.uniqueStations)
                 .enter().append("circle").attr("fill", "red")
-                .attr("cx", d => (d.first.latLong.lon + lonOffset) * scaleLon)
-                .attr("cy", d => height-((d.first.latLong.lat + latOffset) * scaleLat))
-                .attr("r", 5).attr("title", d => d.first.name);
-            mapApp.svg.selectAll("circle")
-                .data(mapApp.positions.positionsList)
-                .enter().append("circle").attr("fill", "red")
-                .attr("cx", d => (d.second.latLong.lon + lonOffset) * scaleLon)
-                .attr("cy", d => height-((d.second.latLong.lat + latOffset) * scaleLat))
-                .attr("r", 5).attr("title", d => d.second.name);
+                .attr("cx", mapApp.scaleX)
+                .attr("cy", mapApp.scaleY)
+                .attr("r", 3).attr("title", d => d.name).attr("id", d=> d.id);
+        },
+        scaleY(station) {
+            return height - ((station.latLong.lat + mapApp.latOffset) * mapApp.scaleLat) + (margin / 2);
+        },
+        scaleX(station) {
+            return ((station.latLong.lon + mapApp.lonOffset) * mapApp.scaleLon) + (margin / 2);
         }
     },
     mounted () {
@@ -75,8 +93,9 @@ const mapApp = new Vue({
             .get('/api/positions')
             .then(function (response) {
                 mapApp.networkError = false;
-                mapApp.positions = response.data;
-                mapApp.draw(mapApp.positions);
+                mapApp.positionsList = response.data.positionsList;
+                mapApp.filterPositions();
+                mapApp.draw();
             })
             .catch(function (error) {
                 mapApp.networkError = true;
