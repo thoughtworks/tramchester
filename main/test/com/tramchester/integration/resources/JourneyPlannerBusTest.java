fb4a495a9b89f532d5a51fdd4d996fa39bc6a7ec
cartwrightian <cartwright.ian@gmail.com>
2020-05-08 16:36:30 +0100

    Make sure to continue to ignore bus tests for now

--- main/test/com/tramchester/integration/resources/JourneyPlannerBusTest.java
+++ main/test/com/tramchester/integration/resources/JourneyPlannerBusTest.java
@@ -31,7 +31,7 @@ import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-//@Ignore("Experimental")
+@Ignore("Experimental")
 public class JourneyPlannerBusTest {
 
     @Rule
