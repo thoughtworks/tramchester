58b9380bc66fe09ada439f1d8ef185bd5b9a1eef
cartwrightian <cartwright.ian@gmail.com>
2020-10-08 16:33:40 +0100

    new timetable data

--- main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
+++ main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
@@ -18,8 +18,8 @@ import java.time.format.DateTimeFormatter;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class DataVersionResourceTest {
-    public static LocalDate validFrom = LocalDate.of(2020, 9, 16); // year, month, day
-    public static LocalDate validUntil = LocalDate.of(2020, 11, 16);
+    public static LocalDate validFrom = LocalDate.of(2020, 10, 5); // year, month, day
+    public static LocalDate validUntil = LocalDate.of(2020, 12, 5);
 
     private static final IntegrationAppExtension appExtension = new IntegrationAppExtension(App.class, new IntegrationTramTestConfig());
 
