3aa8d59854b2a72a27eb23bdd1c177631ba87256
cartwrightian <cartwright.ian@gmail.com>
2020-11-04 20:59:01 +0000

    Update s3 code to aws sdk 2

--- main/src/com/tramchester/cloud/ClientForS3.java
+++ main/src/com/tramchester/cloud/ClientForS3.java
@@ -1,18 +1,17 @@
 package com.tramchester.cloud;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.PutObjectResult;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.tramchester.config.TramchesterConfig;
 import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
@@ -20,12 +19,12 @@ public class ClientForS3 {
     private static final Logger logger = LoggerFactory.getLogger(ClientForS3.class);
 
     private final TramchesterConfig config;
-    private AmazonS3 s3Client;
+    private S3Client s3Client;
 
     public ClientForS3(TramchesterConfig config) {
         this.config = config;
         try {
-            s3Client = AmazonS3ClientBuilder.defaultClient();
+            s3Client = S3Client.create();
         }
         catch (com.amazonaws.SdkClientException exception) {
             logger.warn("Unable to init S3 client, no live data will be archived.");
@@ -37,46 +36,50 @@ public class ClientForS3 {
         logger.debug(format("Uploading to bucket '%s' key '%s' contents '%s'", bucket, key, json));
 
         try {
-            if (!s3Client.doesBucketExistV2(bucket)) {
+            if (!bucketExists(bucket)) {
                 logger.warn(format("Bucket %s does not exist", bucket));
                 return false;
             }
 
             MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-            String localMd5 = Base64.encodeBase64String(messageDigest.digest(json.getBytes()));
+            byte[] bytes = json.getBytes();
+            String localMd5 = Base64.encodeBase64String(messageDigest.digest(bytes));
 
             logger.debug("Uploading with MD5: " + localMd5);
-            PutObjectResult result = s3Client.putObject(bucket, key, json);
-
-            String remoteMd5 = result.getContentMd5();
-            if (localMd5.equals(remoteMd5)) {
-                return true;
-            }
-
-            logger.warn(format("Problem with upload, md5 mismatch. expected '%s' got '%s' ", localMd5, remoteMd5));
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder().bucket(bucket).key(key).contentMD5(localMd5).build(); ;
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(bytes));
         }
-        catch (AmazonS3Exception | NoSuchAlgorithmException exception) {
+        catch (S3Exception | NoSuchAlgorithmException exception) {
             logger.warn(format("Unable to upload to bucket '%s' key '%s'", bucket, key), exception);
+            return false;
         }
-        return false;
+        return true;
+    }
+
+    private boolean bucketExists(String bucket) {
+        ListBucketsResponse listBucketsResponse = s3Client.listBuckets();
+        Set<String> buckets = listBucketsResponse.buckets().stream().map(Bucket::name).collect(Collectors.toSet());
+        return buckets.contains(bucket);
     }
 
     public boolean keyExists(String prefix, String key) {
         String bucket = config.getLiveDataS3Bucket();
         try {
-            if (!s3Client.doesBucketExistV2(bucket)) {
+            if (!bucketExists(bucket)) {
                 logger.error(format("Bucket %s does not exist", bucket));
                 return false;
             }
             // limit by prefix to avoid very large requests
-            ObjectListing summary = s3Client.listObjects(bucket, prefix);
-            for (S3ObjectSummary item : summary.getObjectSummaries()) {
-                if (key.equals(item.getKey())) {
+            ListObjectsRequest listObsRequest = ListObjectsRequest.builder().bucket(bucket).prefix(prefix).build();
+            ListObjectsResponse summary = s3Client.listObjects(listObsRequest);
+
+            for (S3Object item : summary.contents()) {
+                if (key.equals(item.key())) {
                     return true;
                 }
             }
         }
-        catch (AmazonS3Exception exception) {
+        catch (S3Exception exception) {
             logger.warn(format("Cannot check if key '%s' exists in bucket '%s' reason '%s'",
                     key, bucket, exception.getMessage()));
         }
--- main/test/com/tramchester/integration/cloud/ClientForS3Test.java
+++ main/test/com/tramchester/integration/cloud/ClientForS3Test.java
@@ -1,28 +1,37 @@
 package com.tramchester.integration.cloud;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.*;
-import com.amazonaws.waiters.Waiter;
-import com.amazonaws.waiters.WaiterParameters;
 import com.tramchester.cloud.ClientForS3;
 import com.tramchester.testSupport.TestEnv;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.waiters.S3Waiter;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ClientForS3Test {
 
     private static final String TEST_BUCKET_NAME = "tramchestertestlivedatabucket";
     private static final String PREFIX = "test";
     private static final String KEY = PREFIX+"/"+"key";
-    private AmazonS3 s3;
+    private S3Client s3;
     private ClientForS3 clientForS3;
+    private S3Waiter s3Waiter;
 
     @BeforeEach
     void beforeEachTestRuns() {
-        s3 = AmazonS3ClientBuilder.defaultClient();
+        s3Waiter = S3Waiter.create();
+        s3 = S3Client.builder().build();
         clientForS3 = new ClientForS3(TestEnv.GET());
         tidyBucket();
     }
@@ -30,7 +39,8 @@ class ClientForS3Test {
     @AfterEach
     void afterEachTestRuns() {
         tidyBucket();
-        s3.shutdown();
+        s3Waiter.close();
+        s3.close();
     }
 
     @Test
@@ -42,13 +52,21 @@ class ClientForS3Test {
     @Test
     void checkForObjectExisting() {
         createTestBucketAndWait();
-        s3.putObject(TEST_BUCKET_NAME, KEY, "contents");
-        s3.waiters().objectExists().run(new WaiterParameters<>(new GetObjectMetadataRequest(TEST_BUCKET_NAME, KEY)));
+
+        PutObjectRequest request = PutObjectRequest.builder().bucket(TEST_BUCKET_NAME).key(KEY).build();
+        s3.putObject(request, RequestBody.fromString("contents"));
+
+        HeadObjectRequest objectRequest = HeadObjectRequest.builder().bucket(TEST_BUCKET_NAME).key(KEY).build();
+        s3Waiter.waitUntilObjectExists(objectRequest);
         Assertions.assertTrue(clientForS3.keyExists(PREFIX, KEY), "exists"); //waiter will throw if times out
 
-        s3.deleteObject(TEST_BUCKET_NAME, KEY);
-        s3.waiters().objectNotExists().run(new WaiterParameters<>(new GetObjectMetadataRequest(TEST_BUCKET_NAME, KEY)));
+        DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder().bucket(TEST_BUCKET_NAME).key(KEY).build();
+        s3.deleteObject(deleteRequest);
+
+        s3Waiter.waitUntilObjectNotExists(objectRequest);
+
         Assertions.assertFalse(clientForS3.keyExists(PREFIX, KEY), "deleted");
+
     }
 
     private void validateUpload() throws IOException {
@@ -57,75 +75,89 @@ class ClientForS3Test {
         Assertions.assertTrue(uploaded, "uploaded");
 
         // the different S3 clients involved get out of sync, so the one sees the bucket and the the other does not
-        Waiter<HeadBucketRequest> waiter = s3.waiters().bucketExists();
-        waiter.run(new WaiterParameters<>(new HeadBucketRequest(TEST_BUCKET_NAME)));
+        HeadBucketRequest existsRequest = HeadBucketRequest.builder().bucket(TEST_BUCKET_NAME).build();
+        s3Waiter.waitUntilBucketExists(existsRequest);
 
-        ObjectListing currentContents = s3.listObjects(TEST_BUCKET_NAME);
-        List<S3ObjectSummary> summary = currentContents.getObjectSummaries();
-        Assertions.assertEquals(1, summary.size());
-        String key = summary.get(0).getKey();
-        Assertions.assertEquals("test/key", key);
 
-        S3Object theObject = s3.getObject(TEST_BUCKET_NAME, KEY);
+        ListObjectsRequest listRequest = ListObjectsRequest.builder().bucket(TEST_BUCKET_NAME).build();
+        ListObjectsResponse currentContents = s3.listObjects(listRequest);
+
+        List<S3Object> summary = currentContents.contents();
+
+        assertEquals(1, summary.size());
+        String key = summary.get(0).key();
+
+        assertEquals("test/key", key);
+
+        GetObjectRequest getRequest = GetObjectRequest.builder().bucket(TEST_BUCKET_NAME).key(KEY).build();
+
+        ResponseInputStream<GetObjectResponse> theObject = s3.getObject(getRequest);
 
         byte[] target = new byte[contents.length()];
-        theObject.getObjectContent().read(target);
+        theObject.read(target);
 
-        String storedJson = new String(target);
-        Assertions.assertEquals(contents, storedJson);
+        String result = new String(target);
+        assertEquals(contents, result);
     }
 
     void createTestBucketAndWait() {
-        s3.createBucket(TEST_BUCKET_NAME);
-        Waiter<HeadBucketRequest> waiter = s3.waiters().bucketExists();
-        waiter.run(new WaiterParameters<>(new HeadBucketRequest(TEST_BUCKET_NAME)));
+        CreateBucketRequest createRequest = CreateBucketRequest.builder().bucket(TEST_BUCKET_NAME).build();
+        s3.createBucket(createRequest);
+        S3Waiter s3Waiter = S3Waiter.create();
+
+        HeadBucketRequest bucketExists = HeadBucketRequest.builder().bucket(TEST_BUCKET_NAME).build();
+        s3Waiter.waitUntilBucketExists(bucketExists);
+
+        s3Waiter.close();
     }
 
     private void tidyBucket() {
         // due to eventual consistency and unrealability of tests this has become a bit belt and braces :-(
-        if (s3.doesBucketExistV2(TEST_BUCKET_NAME)) {
 
-            ObjectListing listing = s3.listObjects(TEST_BUCKET_NAME);
-            List<S3ObjectSummary> items = listing.getObjectSummaries();
+        ListBucketsResponse listBucketsResponse = s3.listBuckets();
+        Set<String> buckets = listBucketsResponse.buckets().stream().map(Bucket::name).collect(Collectors.toSet());
+        if (buckets.contains(TEST_BUCKET_NAME)) {
 
-            items.forEach(item -> {
-                String key = item.getKey();
-                DeleteObjectsRequest deleteObjects = new DeleteObjectsRequest(TEST_BUCKET_NAME).withKeys(key);
-                try {
-                    s3.deleteObjects(deleteObjects);
-                    Waiter<GetObjectMetadataRequest> keyWaiter = s3.waiters().objectNotExists();
-                    keyWaiter.run(new WaiterParameters<>(new GetObjectMetadataRequest(TEST_BUCKET_NAME, key)));
-                }
-                catch (AmazonS3Exception deletefailed) {
-                    if (deletefailed.getStatusCode()==404) {
-                        // fine, no need to delete
-                    } else {
-                        throw deletefailed;
-                    }
-                }
-            });
+            ListObjectsRequest listRequest;
+            listRequest = ListObjectsRequest.builder().bucket(TEST_BUCKET_NAME).build();
+            ListObjectsResponse listObjsResponse = s3.listObjects(listRequest);
+            List<S3Object> items = listObjsResponse.contents();
 
-            //// KEY
-//            DeleteObjectsRequest deleteObjects = new DeleteObjectsRequest(TEST_BUCKET_NAME).withKeys(KEY);
+            if (!items.isEmpty()) {
+                Set<ObjectIdentifier> toDelete = items.stream().map(item -> ObjectIdentifier.builder().key(item.key()).build()).collect(Collectors.toSet());
+                Delete build = Delete.builder().objects(toDelete).quiet(false).build();
+                DeleteObjectsRequest deleteObjectsRequest = DeleteObjectsRequest.builder().bucket(TEST_BUCKET_NAME).delete(build).build();
+                s3.deleteObjects(deleteObjectsRequest);
 
-//            Waiter<GetObjectMetadataRequest> keyWaiter = s3.waiters().objectNotExists();
-//            keyWaiter.run(new WaiterParameters<>(new GetObjectMetadataRequest(TEST_BUCKET_NAME, KEY)));
+                items.forEach(item -> {
+                    s3Waiter.waitUntilObjectNotExists(HeadObjectRequest.builder().bucket(TEST_BUCKET_NAME).key(item.key()).build());
+                });
+            }
 
             //// BUCKET
             try {
-                s3.deleteBucket(TEST_BUCKET_NAME);
+                DeleteBucketRequest deleteRequest = DeleteBucketRequest.builder().bucket(TEST_BUCKET_NAME).build();
+                s3.deleteBucket(deleteRequest);
             }
-            catch (AmazonS3Exception deletefailed) {
-                if (deletefailed.getStatusCode()==404) {
+            catch (S3Exception deletefailed) {
+                if (deletefailed.statusCode()==404) {
+                    return;
                     // fine, no need to delete
                 } else {
                     throw deletefailed;
                 }
             }
 
-            Waiter<HeadBucketRequest> bucketWaiter = s3.waiters().bucketNotExists();
-            WaiterParameters<HeadBucketRequest> waiterParameters = new WaiterParameters<>(new HeadBucketRequest(TEST_BUCKET_NAME));
-            bucketWaiter.run(waiterParameters);
+            try {
+                s3.headBucket(HeadBucketRequest.builder().bucket(TEST_BUCKET_NAME).build());
+            }
+            catch (S3Exception possibilyAlreadyGone) {
+                if (possibilyAlreadyGone.statusCode()==404) {
+                    return;
+                }
+            }
+
+            s3Waiter.waitUntilBucketNotExists(HeadBucketRequest.builder().bucket(TEST_BUCKET_NAME).build());
         }
     }
 
