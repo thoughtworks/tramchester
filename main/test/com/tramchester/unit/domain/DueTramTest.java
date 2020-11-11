6b34f1178bb46a06f7e3cc6ad02ed64923b32b8d
cartwrightian <cartwright.ian@gmail.com>
2020-11-11 08:29:12 +0000

    Make sure duetrams respects next day flag

---
+++ main/test/com/tramchester/unit/domain/DueTramTest.java
@@ -0,0 +1,34 @@
+package com.tramchester.unit.domain;
+
+import com.tramchester.domain.liveUpdates.DueTram;
+import com.tramchester.domain.time.TramTime;
+import com.tramchester.testSupport.TramStations;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DueTramTest {
+
+    @Test
+    void calculateWhenCorrectly() {
+        LocalTime updateTime = LocalTime.of(10,42);
+        DueTram dueTram = new DueTram(TramStations.of(TramStations.Bury), "Due", 4, "Double", updateTime);
+
+        TramTime result = dueTram.getWhen();
+        assertEquals(updateTime.plusMinutes(4), result.asLocalTime());
+        assertFalse(result.isNextDay());
+
+    }
+
+    @Test
+    void calculateWhenCorrectAcrossMidnight() {
+        LocalTime updateTime = LocalTime.of(23,58);
+        DueTram dueTram = new DueTram(TramStations.of(TramStations.Bury), "Due", 4, "Double", updateTime);
+
+        TramTime result = dueTram.getWhen();
+        assertEquals(LocalTime.of(0,2), result.asLocalTime());
+        assertTrue(result.isNextDay());
+    }
+}
