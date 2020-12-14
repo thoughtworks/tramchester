aa93195d6b452f31fdb8cdcbf6d4e9a93146df64
cartwrightian <cartwright.ian@gmail.com>
2020-12-14 11:16:58 +0000

    v. small code tidy

--- main/src/com/tramchester/mappers/serialisation/LocalDateTimeJsonSerializer.java
+++ main/src/com/tramchester/mappers/serialisation/LocalDateTimeJsonSerializer.java
@@ -11,7 +11,7 @@ import java.time.format.DateTimeFormatter;
 public class LocalDateTimeJsonSerializer extends JsonSerializer<LocalDateTime> {
 
     @Override
-    public void serialize(LocalDateTime time, JsonGenerator gen, SerializerProvider arg2) throws IOException {
+    public void serialize(LocalDateTime time, JsonGenerator gen, SerializerProvider serializerProvider) throws IOException {
 
         gen.writeString(time.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
     }
