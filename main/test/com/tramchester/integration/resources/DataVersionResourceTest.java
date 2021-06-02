5e2703c0bf27e770415bb1ab9e0d04bf8b7cae9f
cartwrightian <cartwright.ian@gmail.com>
2021-06-02 10:54:41 +0100

    one line version num change for latest data

--- main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
+++ main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class DataVersionResourceTest {
 
-    public static String version = "20210527_08_58_26";
+    public static String version = "20210531_22_36_18";
 
     private static final IntegrationAppExtension appExtension = new IntegrationAppExtension(App.class, new IntegrationTramTestConfig());
 
