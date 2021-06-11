e8c282f952e3bbaeae17f9d05575b35ca39353b8
cartwrightian <cartwright.ian@gmail.com>
2021-06-11 15:13:36 +0100

    just the last 5 lines of logfile

--- deploy/start.sh
+++ deploy/start.sh
@@ -8,8 +8,8 @@ LOGFILE=/home/ec2-user/server/logs/tramchester_local.log
 until ./$target/bin/tramchester server ./$target/config/local.yml 1> /dev/null; do
     logger ERROR tramchester Stopped
     if [ -f $LOGFILE ]; then
-      logger tramchester last 10 lines of $LOGFILE
-      tail -10 $LOGFILE | logger
+      logger tramchester last 5 lines of $LOGFILE
+      tail -5 $LOGFILE | logger
     fi
     sleep 15
     logger ERROR tramchester Restarting
