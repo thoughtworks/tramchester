c9a1366ce927d20af1ebec0c2b43a3fa823f4a52
cartwrightian <cartwright.ian@gmail.com>
2021-07-28 10:28:22 +0100

    New data version

--- main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
+++ main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class DataVersionResourceTest {
 
-    public static final String version = "20210727_02_30_05";
+    public static final String version = "20210728_08_59_15";
 
     private static final IntegrationAppExtension appExtension = new IntegrationAppExtension(App.class, new IntegrationTramTestConfig());
 
