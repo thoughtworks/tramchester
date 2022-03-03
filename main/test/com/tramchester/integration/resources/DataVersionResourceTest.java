ee1420e1f7fb4f59c3bf2eb391ff23c4c71fac11
cartwrightian <cartwright.ian@gmail.com>
2022-03-03 11:02:09 +0000

    date/version change on tfgm data

--- main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
+++ main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class DataVersionResourceTest {
 
-    public static final String version = "20220302_13_50_47";
+    public static final String version = "20220303_09_43_19";
 
     private static final IntegrationAppExtension appExtension = new IntegrationAppExtension(App.class,
             new ResourceTramTestConfig<>(DataVersionResource.class));
