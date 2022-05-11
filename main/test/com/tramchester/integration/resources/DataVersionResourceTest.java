8101eb51392379738a097d2bc7ee76770bb8d64e
Ian Cartwright <cartwright.ian@gmail.com>
2022-05-11 14:37:51 +0100

    latest data, turn live data tests back on, api seems to be up again

--- main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
+++ main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class DataVersionResourceTest {
 
-    public static final String version = "20220510_10_58_40";
+    public static final String version = "20220511_02_30_04";
 
     private static final IntegrationAppExtension appExtension = new IntegrationAppExtension(App.class,
             new ResourceTramTestConfig<>(DataVersionResource.class));
