94cd92e7989fd6d0770069ac2db03c70213418f6
Ian Cartwright <cartwright.ian@gmail.com>
2022-07-19 16:42:10 +0100

    New data version

--- main/test/com/tramchester/integration/repository/buses/TransportDataFromFilesBusTest.java
+++ main/test/com/tramchester/integration/repository/buses/TransportDataFromFilesBusTest.java
@@ -41,7 +41,7 @@ public
 class TransportDataFromFilesBusTest {
 
     public static final int TGFM_BUS_AGENCIES = 41;
-    public static final int TGFM_BUS_ROUTES = 1492;
+    public static final int TGFM_BUS_ROUTES = 1575;
     public static final int NUM_TFGM_BUS_STATIONS = 15759;
     private static ComponentContainer componentContainer;
     private static TramchesterConfig config;
