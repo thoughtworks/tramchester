d7edfb566d8b152d78c19bb0ada38237340b9e4a
Ian Cartwright <cartwright.ian@gmail.com>
2022-08-24 14:22:48 +0100

    Move date overlap tests from RouteTest to AggregateCalendarServiceTest

--- main/test/com/tramchester/unit/domain/dates/AggregateServiceCalendarTest.java
+++ main/test/com/tramchester/unit/domain/dates/AggregateServiceCalendarTest.java
@@ -1,24 +1,25 @@
 package com.tramchester.unit.domain.dates;
 
+import com.tramchester.domain.MutableRoute;
+import com.tramchester.domain.Service;
 import com.tramchester.domain.dates.*;
+import com.tramchester.domain.id.StringIdFor;
 import com.tramchester.testSupport.TestEnv;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import org.neo4j.cypher.internal.expressions.functions.E;
 
 import java.time.DayOfWeek;
-import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
 
-import static java.time.DayOfWeek.THURSDAY;
-import static java.time.DayOfWeek.TUESDAY;
+import static java.time.DayOfWeek.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AggregateServiceCalendarTest {
 
     private EnumSet<DayOfWeek> allDays;
+    private final EnumSet<DayOfWeek> noDays = EnumSet.noneOf(DayOfWeek.class);
 
     @BeforeEach
     void setUp() {
@@ -102,7 +103,337 @@ public class AggregateServiceCalendarTest {
             }
             date = date.plusDays(1);
         }
+    }
 
+    @Test
+    void shouldRespectDateRangesOnServicesNoOverlap() {
+        final TramDate startDateA = TramDate.of(2020, 11, 5);
+        final TramDate endDateA = TramDate.of(2020, 11, 25);
+
+        final TramDate startDateB = TramDate.of(2020, 12, 5);
+        final TramDate endDateB = TramDate.of(2020, 12, 25);
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(DateRange.of(startDateA, endDateA), allDays);
+
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(DateRange.of(startDateB, endDateB), allDays);
+
+        AggregateServiceCalendar aggregateServiceCalendar = new AggregateServiceCalendar(Arrays.asList(calendarA, calendarB));
+
+        assertFalse(aggregateServiceCalendar.operatesOn(startDateA.minusDays(1)));
+        assertFalse(aggregateServiceCalendar.operatesOn(endDateA.plusDays(1)));
+
+        assertFalse(aggregateServiceCalendar.operatesOn(startDateB.minusDays(1)));
+        assertFalse(aggregateServiceCalendar.operatesOn(endDateB.plusDays(1)));
+
+        TramDate date = startDateA;
+        while (date.isBefore(endDateA)) {
+            assertTrue(aggregateServiceCalendar.operatesOn(date), "should be available on " + date);
+            date = date.plusDays(1);
+        }
+
+        date = startDateB;
+        while (date.isBefore(endDateB)) {
+            assertTrue(aggregateServiceCalendar.operatesOn(date), "should be available on " + date);
+            date = date.plusDays(1);
+        }
+    }
+
+    @Test
+    void shouldRespectDateRangesOnServicesWithOverlap() {
+        final TramDate startDateA = TramDate.of(2020, 11, 5);
+        final TramDate endDateA = TramDate.of(2020, 11, 25);
+
+        final TramDate startDateB = endDateA.minusDays(1);
+        final TramDate endDateB = TramDate.of(2020, 12, 25);
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(DateRange.of(startDateA, endDateA), allDays);
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(DateRange.of(startDateB, endDateB), allDays);
+
+        AggregateServiceCalendar route = new AggregateServiceCalendar(Arrays.asList(calendarA, calendarB));
+
+        assertFalse(route.operatesOn(startDateA.minusDays(1)));
+        assertFalse(route.operatesOn(endDateB.plusDays(1)));
+
+        TramDate date = startDateA;
+        while (date.isBefore(endDateB)) {
+            assertTrue(route.operatesOn(date), "should be available on " + date);
+            date = date.plusDays(1);
+        }
+
+    }
+
+    @Test
+    void shouldRespectDateRangesOnServicesWithNoAdditionalDayOverlap() {
+        final TramDate startDate = TramDate.of(2020, 11, 5);
+        final TramDate endDate = TramDate.of(2020, 11, 25);
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(DateRange.of(startDate, endDate), allDays);
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(DateRange.of(startDate, endDate), noDays);
+
+        assertFalse(calendarA.anyDateOverlaps(calendarB));
+        assertFalse(calendarB.anyDateOverlaps(calendarA));
+
+        AggregateServiceCalendar aggregateServiceCalendarA = new AggregateServiceCalendar(Collections.singleton(calendarA));
+        AggregateServiceCalendar aggregateServiceCalendarB = new AggregateServiceCalendar(Collections.singleton(calendarB));
+
+        assertFalse(aggregateServiceCalendarA.anyDateOverlaps(aggregateServiceCalendarB));
+        assertFalse(aggregateServiceCalendarB.anyDateOverlaps(aggregateServiceCalendarA));
+    }
+
+    @Test
+    void shouldRespectDateRangesOnServicesWithAdditionalDayOverlap() {
+        final TramDate startDate = TramDate.of(2020, 11, 5);
+        final TramDate endDate = TramDate.of(2020, 11, 25);
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(DateRange.of(startDate, endDate), allDays);
+
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(DateRange.of(startDate, endDate), noDays);
+        calendarB.includeExtraDate(startDate.plusDays(1));
+
+        assertTrue(calendarA.anyDateOverlaps(calendarB));
+        assertTrue(calendarB.anyDateOverlaps(calendarA));
+
+        AggregateServiceCalendar aggregateServiceCalendarA = new AggregateServiceCalendar(Collections.singleton(calendarA));
+        AggregateServiceCalendar aggregateServiceCalendarB = new AggregateServiceCalendar(Collections.singleton(calendarB));
+
+        assertTrue(aggregateServiceCalendarA.anyDateOverlaps(aggregateServiceCalendarB));
+        assertTrue(aggregateServiceCalendarB.anyDateOverlaps(aggregateServiceCalendarB));
+
+    }
+
+    @Test
+    void shouldRespectDateRangesOnServicesWithAdditionalAndExcludeNegatingAnyOverlap() {
+        final TramDate startDate = TramDate.of(2020, 11, 5);
+        final TramDate endDate = TramDate.of(2020, 11, 25);
+
+        TramDate specialDay = TramDate.of(2020,11,15);
+
+        DateRange dateRange = DateRange.of(startDate, endDate);
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(dateRange, allDays);
+        calendarA.excludeDate(specialDay); // exclude
+
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(dateRange, noDays);
+        calendarB.includeExtraDate(specialDay); // include
+
+        assertFalse(calendarA.anyDateOverlaps(calendarB));
+        assertFalse(calendarB.anyDateOverlaps(calendarA));
+
+        AggregateServiceCalendar aggregateServiceCalendarA = new AggregateServiceCalendar(Collections.singleton(calendarA));
+        AggregateServiceCalendar aggregateServiceCalendarB = new AggregateServiceCalendar(Collections.singleton(calendarB));
+
+        assertFalse(aggregateServiceCalendarA.anyDateOverlaps(aggregateServiceCalendarB));
+        assertFalse(aggregateServiceCalendarB.anyDateOverlaps(aggregateServiceCalendarA));
+
+    }
+
+    @Test
+    void shouldRespectDateRangesOnServicesNoOverlapExcludedDays() {
+        final TramDate startDateA = TramDate.of(2020, 11, 5);
+        final TramDate endDateA = TramDate.of(2020, 11, 25);
+
+        final TramDate startDateB = TramDate.of(2020, 12, 5);
+        final TramDate endDateB = TramDate.of(2020, 12, 25);
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(DateRange.of(startDateA, endDateA), allDays);
+        TramDate exclusionOne = TramDate.of(2020, 11, 10);
+        calendarA.excludeDate(exclusionOne);
+
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(DateRange.of(startDateB, endDateB), allDays);
+        TramDate exclusionTwo = TramDate.of(2020, 12, 10);
+        calendarB.excludeDate(exclusionTwo);
+
+        AggregateServiceCalendar aggregateServiceCalendar = new AggregateServiceCalendar(Arrays.asList(calendarA, calendarB));
+
+        assertFalse(aggregateServiceCalendar.operatesOn(exclusionOne));
+        assertFalse(aggregateServiceCalendar.operatesOn(exclusionTwo));
+    }
+
+    @Test
+    void shouldRespectDateRangesOnServicesIncludedDays() {
+        final TramDate startDate = TramDate.of(2020, 11, 5);
+        final TramDate endDate = TramDate.of(2020, 11, 25);
+
+        DateRange dateRange = DateRange.of(startDate, endDate);
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(dateRange, noDays);
+        TramDate additionOne = TramDate.of(2020, 11, 10);
+        calendarA.includeExtraDate(additionOne);
+
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(dateRange, noDays);
+        TramDate additionTwo = TramDate.of(2020, 11, 12);
+        calendarB.includeExtraDate(additionTwo);
+
+        AggregateServiceCalendar aggregateServiceCalendar = new AggregateServiceCalendar(Arrays.asList(calendarA, calendarB));
+
+        assertTrue(aggregateServiceCalendar.operatesOn(additionOne));
+        assertTrue(aggregateServiceCalendar.operatesOn(additionTwo));
+
+        dateRange.stream().filter(date -> !(date.equals(additionOne) || date.equals(additionTwo))).
+                forEach(date -> assertFalse(aggregateServiceCalendar.operatesOn(date), "should not be availble on " + date));
+
+    }
+
+    @Test
+    void shouldRespectDateRangesOverlapDifferentAdditionalDays() {
+        final TramDate startDate = TramDate.of(2020, 11, 5);
+        final TramDate endDate = TramDate.of(2020, 11, 25);
+
+        DateRange dateRange = DateRange.of(startDate, endDate);
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(dateRange, noDays);
+        TramDate additionOne = TramDate.of(2020, 11, 10);
+        calendarA.includeExtraDate(additionOne);
+
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(dateRange, noDays);
+        TramDate additionTwo = TramDate.of(2020, 11, 12);
+        calendarB.includeExtraDate(additionTwo);
+
+        assertFalse(calendarA.anyDateOverlaps(calendarB));
+        assertFalse(calendarB.anyDateOverlaps(calendarA));
+
+        AggregateServiceCalendar aggregateServiceCalendarA = new AggregateServiceCalendar(Collections.singleton(calendarA));
+        AggregateServiceCalendar aggregateServiceCalendarB = new AggregateServiceCalendar(Collections.singleton(calendarB));
+
+        assertFalse(aggregateServiceCalendarA.anyDateOverlaps(aggregateServiceCalendarB));
+        assertFalse(aggregateServiceCalendarB.anyDateOverlaps(aggregateServiceCalendarA));
+
+    }
+
+    @Test
+    void shouldRespectDateRangesOverlapSameAdditionalDays() {
+        final TramDate startDate = TramDate.of(2020, 11, 5);
+        final TramDate endDate = TramDate.of(2020, 11, 25);
+
+        DateRange dateRange = DateRange.of(startDate, endDate);
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(dateRange, noDays);
+        TramDate addition = TramDate.of(2020, 11, 10);
+        calendarA.includeExtraDate(addition);
+
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(dateRange, noDays);
+        calendarB.includeExtraDate(addition);
+
+        assertTrue(calendarA.anyDateOverlaps(calendarB));
+        assertTrue(calendarB.anyDateOverlaps(calendarA));
+
+        AggregateServiceCalendar aggregateServiceCalendarA = new AggregateServiceCalendar(Collections.singleton(calendarA));
+        AggregateServiceCalendar aggregateServiceCalendarB = new AggregateServiceCalendar(Collections.singleton(calendarB));
+
+        assertTrue(aggregateServiceCalendarA.anyDateOverlaps(aggregateServiceCalendarB));
+        assertTrue(aggregateServiceCalendarB.anyDateOverlaps(aggregateServiceCalendarA));
+    }
+
+    @Test
+    void shouldRespectDateRangesOverlapNotSameWeekdaysWithAddition() {
+        final TramDate startDateA = TramDate.of(2020, 11, 5);
+        final TramDate endDateA = TramDate.of(2020, 11, 25);
+
+        DateRange dateRange = DateRange.of(startDateA, endDateA);
+
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(dateRange, EnumSet.of(MONDAY, TUESDAY));
+        TramDate addition = TramDate.of(2020, 11, 10);
+        calendarA.includeExtraDate(addition);
+
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(dateRange, EnumSet.of(WEDNESDAY, THURSDAY));
+        calendarB.includeExtraDate(addition);
+
+        assertTrue(calendarA.anyDateOverlaps(calendarB));
+        assertTrue(calendarB.anyDateOverlaps(calendarA));
+
+        AggregateServiceCalendar aggregateServiceCalendarA = new AggregateServiceCalendar(Collections.singleton(calendarA));
+        AggregateServiceCalendar aggregateServiceCalendarB = new AggregateServiceCalendar(Collections.singleton(calendarB));
+
+        assertTrue(aggregateServiceCalendarA.anyDateOverlaps(aggregateServiceCalendarB));
+        assertTrue(aggregateServiceCalendarB.anyDateOverlaps(aggregateServiceCalendarA));
+    }
+
+    @Test
+    void shouldRespectDateRangesOverlapNotSameWeekdaysWithOutAddition() {
+        final TramDate startDateA = TramDate.of(2020, 11, 5);
+        final TramDate endDateA = TramDate.of(2020, 11, 25);
+
+        DateRange dateRange = DateRange.of(startDateA, endDateA);
+
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(dateRange, EnumSet.of(MONDAY, TUESDAY));
+
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(dateRange, EnumSet.of(WEDNESDAY, THURSDAY));
+
+        assertFalse(calendarA.anyDateOverlaps(calendarB));
+        assertFalse(calendarB.anyDateOverlaps(calendarA));
+
+        AggregateServiceCalendar aggregateServiceCalendarA = new AggregateServiceCalendar(Collections.singleton(calendarA));
+        AggregateServiceCalendar aggregateServiceCalendarB = new AggregateServiceCalendar(Collections.singleton(calendarB));
+
+        assertFalse(aggregateServiceCalendarA.anyDateOverlaps(aggregateServiceCalendarB));
+        assertFalse(aggregateServiceCalendarB.anyDateOverlaps(aggregateServiceCalendarA));
+    }
+
+    @Test
+    void shouldRespectDateRangesOnServicesInclusionOverridesExclusion() {
+        final TramDate startDateA = TramDate.of(2020, 11, 5);
+        final TramDate endDateA = TramDate.of(2020, 11, 25);
+
+        TramDate specialDay = TramDate.of(2020, 11, 10);
+
+        // does not run on special day
+        MutableServiceCalendar calendarA = new MutableServiceCalendar(DateRange.of(startDateA, endDateA), allDays);
+        calendarA.excludeDate(specialDay);
+
+        // runs ONLY on special day
+        MutableServiceCalendar calendarB = new MutableServiceCalendar(DateRange.of(startDateA, endDateA), noDays);
+        calendarB.includeExtraDate(specialDay);
+
+        AggregateServiceCalendar aggregateServiceCalendar = new AggregateServiceCalendar(Arrays.asList(calendarA, calendarB));
+
+        assertTrue(aggregateServiceCalendar.operatesOn(specialDay));
+
+    }
+
+    @Test
+    void shouldRespectAdditionalDaysOnService() {
+        TramDate startDate = TramDate.of(2020, 11, 5);
+        TramDate endDate = TramDate.of(2020, 11, 25);
+
+        MutableServiceCalendar calendar = new MutableServiceCalendar(DateRange.of(startDate, endDate), noDays);
+        TramDate extraRunningDate = TramDate.of(2020, 11, 10);
+        calendar.includeExtraDate(extraRunningDate);
+
+        assertTrue(calendar.operatesOn(extraRunningDate));
+
+        AggregateServiceCalendar aggregateServiceCalendar = new AggregateServiceCalendar(Collections.singleton(calendar));
+
+        assertTrue(aggregateServiceCalendar.operatesOn(extraRunningDate));
+    }
+
+    @Test
+    void shouldHaveDateOverlap() {
+
+        TramDate startDate = TramDate.of(2020, 11, 5);
+        TramDate endDate = TramDate.of(2020, 11, 25);
+
+        EnumSet<DayOfWeek> monday = EnumSet.of(MONDAY);
+
+        ServiceCalendar serviceA = createService(startDate, endDate, "serviceA", monday);
+        ServiceCalendar serviceB = createService(startDate, endDate, "serviceB", EnumSet.of(DayOfWeek.SUNDAY));
+        ServiceCalendar serviceC = createService(startDate.minusDays(10), startDate.minusDays(5), "serviceC", monday);
+        ServiceCalendar serviceD = createService(startDate, endDate, "serviceD", monday);
+
+        assertTrue(serviceA.anyDateOverlaps(serviceA));
+
+        // wrong operating days
+        assertFalse(serviceA.anyDateOverlaps(serviceB));
+
+        // before dates
+        assertFalse(serviceA.anyDateOverlaps(serviceC));
+
+        // should match
+        assertTrue(serviceA.anyDateOverlaps(serviceD));
+    }
+
+    private ServiceCalendar createService(TramDate startDate, TramDate endDate, String service, EnumSet<DayOfWeek> dayOfWeeks) {
+        return new MutableServiceCalendar(DateRange.of(startDate, endDate), dayOfWeeks);
     }
 
 }
