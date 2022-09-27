a6dac9c9916b6e49b5ed19354117c0c3408bd60d
Ian Cartwright <cartwright.ian@gmail.com>
2022-09-27 17:21:01 +0100

    More quick version housekeeping.

--- main/test/com/tramchester/acceptance/infra/ProvidesChromeDriver.java
+++ main/test/com/tramchester/acceptance/infra/ProvidesChromeDriver.java
@@ -17,6 +17,10 @@ import java.util.logging.Level;
 import static org.openqa.selenium.chrome.ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY;
 import static org.openqa.selenium.chrome.ChromeDriverService.CHROME_DRIVER_VERBOSE_LOG_PROPERTY;
 
+
+// Fix binary verification issue
+// https://timonweb.com/misc/fixing-error-chromedriver-cannot-be-opened-because-the-developer-cannot-be-verified-unable-to-launch-the-chrome-browser-on-mac-os/
+
 public class ProvidesChromeDriver extends ProvidesDesktopDriver {
     public final static String Name = "chrome";
     private final DesiredCapabilities capabilities;
