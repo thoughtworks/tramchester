d8d504f2f165dbb6879d76fc63cb4463ede0c891
Ian Cartwright <cartwright.ian@gmail.com>
2022-11-24 12:07:31 +0000

    forgot to disable gm config tests on all journeys tests again

--- main/test/com/tramchester/integration/graph/RouteCalculatorTestAllJourneys.java
+++ main/test/com/tramchester/integration/graph/RouteCalculatorTestAllJourneys.java
@@ -36,7 +36,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(ConfigParameterResolver.class)
 @DualTest
-@GMTest
 class RouteCalculatorTestAllJourneys {
 
     private static ComponentContainer componentContainer;
