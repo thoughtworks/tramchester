df83c629861b72fd4eabdb843d5001aea0ab5c1e
Ian Cartwright <cartwright.ian@gmail.com>
2023-05-01 14:47:16 +0100

    data version update

--- main/test/com/tramchester/integration/repository/buses/TransportDataFromFilesBusTest.java
+++ main/test/com/tramchester/integration/repository/buses/TransportDataFromFilesBusTest.java
@@ -42,7 +42,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public
 class TransportDataFromFilesBusTest {
 
-    public static final int TGFM_BUS_AGENCIES = 42;
+    public static final int TGFM_BUS_AGENCIES = 41;
     public static final int TGFM_BUS_ROUTES = 1345;
     public static final int NUM_TFGM_BUS_STATIONS = 15697;
     private static ComponentContainer componentContainer;
